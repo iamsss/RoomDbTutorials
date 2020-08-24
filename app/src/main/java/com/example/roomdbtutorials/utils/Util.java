e446481bb5637cbfc1e11d397302161106efb1cf
iamsss <saurav9760@gmail.com>
2020-08-24 19:23:43 +0530

    Adding Util Class

---
+++ app/src/main/java/com/example/roomdbtutorials/utils/Util.java
@@ -0,0 +1,6 @@
+package com.example.roomdbtutorials.utils;
+
+public class Util {
+    public static final String DATABASE_NAME = "NoteDb";
+    public static final String TABLE_NAME_NOTE = "Note";
+}
